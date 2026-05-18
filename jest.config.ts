3e6c6e1bb03183625564d03273b5c45d7fe0bc92
Dps <dps@Dpss-MacBook-Pro.local>
2026-05-18 13:55:41 +0545

    chore: fix ts-jest v30 and moduleResolution config

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,14 @@
 import type { Config } from 'jest';
 const config: Config = {
   testEnvironment: 'jsdom',
-  transform: { '^.+\\.tsx?$': ['ts-jest', { tsconfig: { jsx: 'react-jsx' } }] },
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: {
+        jsx: 'react-jsx',
+        moduleResolution: 'node',
+      },
+    }],
+  },
   moduleNameMapper: { '^@/(.*)$': '<rootDir>/$1' },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
